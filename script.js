8eb5dbeb89695f4245d3949618f3f83fdac3103f
petimov <petr.movsesjan20@gmail.com>
2025-08-18 00:43:31 +0200

    working, except for the winning functionality. you just cant win or lose

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ for (let i = 0; i < 9; i++) {
     board.innerHTML += '<div class="places"></div>'
 }
 
+let counter = 0;
+
 const places = document.querySelectorAll('.places')
-
-console.log(places)
-
 
 places.forEach(place => {
     place.addEventListener('click', (e) => {
-        place.classList.add('clicked');
+        if (place.classList.contains('clicked')) {
+        } else {
+            place.classList.add('clicked');
+            counter += 1;
+        }
+
+        if (counter % 2 == 0) {
+            place.innerHTML = 'o'
+        } else {
+            place.innerHTML = 'x'
+        }
     })
 
 })
