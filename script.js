07ea7663a989f1b62af671faf7f50241f5e070c2
petimov <petr.movsesjan20@gmail.com>
2025-08-18 10:45:43 +0200

    fully working, but very repetetive code

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ for (let i = 0; i < 9; i++) {
 
 let counter = 0;
 let xWon = false;
+let oWon = false;
 
 const places = document.querySelectorAll('.places')
 
@@ -26,11 +27,46 @@ places.forEach(place => {
         places.forEach(place => {
             if ([...board.children].slice(0, 3).every(cell => cell.innerText === 'x')) {
                 xWon = true;
+            } else if ([...board.children].slice(3, 6).every(cell => cell.innerText === 'x')) {
+                xWon = true;
+            } else if ([...board.children].slice(6, 9).every(cell => cell.innerText === 'x')) {
+                xWon = true;
+            } else if (board.children[0].innerText == 'x' && board.children[3].innerText == 'x' && board.children[6].innerText == 'x') {
+                xWon = true;
+            } else if (board.children[1].innerText == 'x' && board.children[4].innerText == 'x' && board.children[7].innerText == 'x') {
+                xWon = true;
+            } else if (board.children[2].innerText == 'x' && board.children[5].innerText == 'x' && board.children[8].innerText == 'x') {
+                xWon = true;
+            } else if (board.children[0].innerText == 'x' && board.children[4].innerText == 'x' && board.children[8].innerText == 'x') {
+                xWon = true;
+            } else if (board.children[2].innerText == 'x' && board.children[4].innerText == 'x' && board.children[6].innerText == 'x') {
+                xWon = true;
             }
+
+            else if ([...board.children].slice(0, 3).every(cell => cell.innerText === 'o')) {
+                oWon = true;
+            } else if ([...board.children].slice(3, 6).every(cell => cell.innerText === 'o')) {
+                oWon = true;
+            } else if ([...board.children].slice(6, 9).every(cell => cell.innerText === 'o')) {
+                oWon = true;
+            } else if (board.children[0].innerText == 'o' && board.children[3].innerText == 'o' && board.children[6].innerText == 'o') {
+                oWon = true;
+            } else if (board.children[1].innerText == 'o' && board.children[4].innerText == 'o' && board.children[7].innerText == 'o') {
+                oWon = true;
+            } else if (board.children[2].innerText == 'o' && board.children[5].innerText == 'o' && board.children[8].innerText == 'o') {
+                oWon = true;
+            } else if (board.children[0].innerText == 'o' && board.children[4].innerText == 'o' && board.children[8].innerText == 'o') {
+                oWon = true;
+            } else if (board.children[2].innerText == 'o' && board.children[4].innerText == 'o' && board.children[6].innerText == 'o') {
+                oWon = true;
+            }
+
         })
 
         if (xWon) {
             alert('x won')
+        } else if (oWon) {
+            alert('o won')
         }
     })
 
